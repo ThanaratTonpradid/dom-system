c8334cacbce1107f61fb5d5003b036a9993327a7
Thanarat Tonpradit <auauone@gmail.com>
2020-01-01 18:20:59 +0700

    refactor(router) add router file

---
+++ src/router/routes.ts
@@ -0,0 +1,14 @@
+import { RouteConfig } from 'vue-router';
+
+export default [
+  {
+    path: '/',
+    name: 'home',
+    component: () => import('../views/Home.vue'),
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: () => import('../views/About.vue'),
+  },
+] as RouteConfig[];
