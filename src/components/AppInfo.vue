bc446a213d4e2b6f4a473a6ee630077ae8d0a910
Thanarat Tonpradit <auauone@gmail.com>
2020-01-04 15:40:14 +0700

    wip(layout) add main custom layout

---
+++ src/components/AppInfo.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app-bar dark app clipped-left>
+    <v-toolbar-title>Page title</v-toolbar-title>
+    <v-spacer></v-spacer>
+  </v-app-bar>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { Component } from 'vue-property-decorator';
+
+@Component
+export default class AppInfo extends Vue {}
+</script>
--- src/layouts/AppLayout.vue
+++ src/layouts/AppLayout.vue
@@ -1,10 +1,48 @@
 <template>
-  <router-view/>
+  <v-app id="inspire">
+    <AppInfo />
+    <Navbar />
+    <v-content>
+      <v-container class="fill-height">
+        <v-row justify="center" align="center">
+          <v-col class="shrink">
+            <router-view />
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+import Vue from 'vue';
+import { Component } from 'vue-property-decorator';
+import Navbar from '../components/Navbar.vue';
+import AppInfo from '../components/AppInfo.vue';
 
-@Component
-export default class AppLayout extends Vue {}
+@Component({
+  components: {
+    AppInfo,
+    Navbar,
+  },
+})
+export default class AppLayout extends Vue {
+  drawer = false;
+
+  items = [
+    { icon: 'fas fa-trending_up', text: 'Most Popular' },
+    { icon: 'fas fa-subscriptions', text: 'Subscriptions' },
+    { icon: 'fas fa-history', text: 'History' },
+    { icon: 'fas fa-featured_play_list', text: 'Playlists' },
+    { icon: 'fas fa-watch_later', text: 'Watch Later' },
+  ];
+
+  items2 = [
+    { picture: 28, text: 'Joseph' },
+    { picture: 38, text: 'Apple' },
+    { picture: 48, text: 'Xbox Ahoy' },
+    { picture: 58, text: 'Nokia' },
+    { picture: 78, text: 'MKBHD' },
+  ];
+}
 </script>
